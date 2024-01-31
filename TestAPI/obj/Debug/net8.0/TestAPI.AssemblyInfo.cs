11664cf87ee926476cdca02593a0d1e79c6ce81a
N1keshLama <144378637+N1keshLama@users.noreply.github.com>
2024-01-31 18:17:49 +0000

    Calculator done!!

--- TestAPI/obj/Debug/net8.0/TestAPI.AssemblyInfo.cs
+++ TestAPI/obj/Debug/net8.0/TestAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5691dc199b396944704dd988e8c0ab79f90d8469")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ef78875bad99bfcb1e4e343a69e0bd463d2a4a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
